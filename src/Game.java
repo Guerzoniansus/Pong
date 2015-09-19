dc45d4e629cee3e508306708d8157a7f570a4e58
Nomar <weezimonkey@gmail.com>
2015-09-19 16:39:27 +0200

    hello

---
+++ src/Game.java
@@ -0,0 +1,137 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+
+public class Game extends Canvas implements Runnable {
+	
+	Game game;
+	
+	Graphics graphics;
+	
+	int roomWidth;
+	int roomHeight;
+	
+	Image wall, bat, ball;
+	
+	int batHeight = 64;
+	int batWidth = 16;
+	
+	Bat player, ai;
+	
+	boolean isRunning;
+	
+	public Game(int roomWidth, int roomHeight) {
+		
+		this.roomWidth = roomWidth;
+		this.roomHeight = roomHeight;
+		
+		try {
+			
+			loadGraphics();			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		this.setSize(roomWidth, roomHeight);
+		this.requestFocus();
+		
+		game = this;
+		
+		game.addMouseListener(new MouseListener());
+		
+		player = new Bat(roomWidth - 70, roomHeight / 2 - 20, bat, game);
+		
+		isRunning = true;
+	}
+	
+	@Override
+	public void run() {
+		
+		while (isRunning) {
+					
+			repaint();
+			
+		}
+		
+	}
+	
+	public void start() {
+		
+		new Thread(this).start();
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		
+		g.drawImage(wall, 0, 0, null);
+		g.drawImage(wall, 0, roomHeight - 46, null);
+		
+		player.draw(g);	
+		
+	}
+	
+	public void loadGraphics() throws IOException {
+		
+		wall = fetchResizedImage("wall.png", roomWidth, 16);
+		bat = fetchResizedImage("wall.png", batWidth, batHeight);
+		
+	}
+	
+	public Image fetchResizedImage(String fileName, int width, int length) throws IOException {
+		// length = x
+		// width = y
+		return fetchImage(fileName).getScaledInstance(width, length, 0);
+
+	}
+
+	public BufferedImage fetchImage(String fileName) throws IOException {
+		return ImageIO.read(this.getClass().getResourceAsStream(fileName));
+
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Game g = new Game(720, 480);
+		g.setBackground(Color.BLACK);
+		
+		
+		JFrame frame = new JFrame("Pong");
+		
+		frame.add(g);
+		frame.setSize(720, 480);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.requestFocus();
+		
+		g.start();
+		
+	}
+	
+	public class MouseListener extends MouseAdapter {
+		
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			
+			double newy = e.getY();
+			
+			player.updatePosition(newy);
+			
+		}
+		
+	}
+
+
+}
