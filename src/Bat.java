12291ad1869450a0cd7decf20912549a15aab668
microsoft <root@Laura.seaweeinc.com>
2015-09-19 12:05:13 -0400

    deleted sys32

--- src/Bat.java
+++ src/Bat.java
@@ -2,34 +2,33 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
 
-
 public class Bat extends Rectangle {
-	
+
 	Image sprite;
 	Game game;
-	
+
 	int x;
 	double y;
-	
+
 	public Bat(int x, int y, Image sprite, Game game) {
-		
+
 		this.game = game;
 		this.sprite = sprite;
 		this.x = x;
-		this.y = y;		
-		
+		this.y = y;
+
 	}
-	
+
 	public void draw(Graphics g) {
-		
-		g.drawImage(sprite, x, (int)Math.round(y), null);
-		
+
+		g.drawImage(sprite, x, (int) Math.round(y), null);
+
 	}
-	
+
 	public void updatePosition(double newy) {
-		
+
 		this.y = newy;
-		
+
 	}
 
 }
